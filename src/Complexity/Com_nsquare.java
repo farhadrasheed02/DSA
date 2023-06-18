ab86fd95ef286cd665390e9a9a0151c916d64d41
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-19 01:15:39 +0530

    Commit by FR

---
+++ src/Complexity/Com_nsquare.java
@@ -0,0 +1,22 @@
+package Complexity;
+
+public class Com_nsquare {
+	
+	public static void n2(int [] numbers)
+	{
+//		for(int num : numbers)
+//		
+//		for(int num1 : numbers)
+//		
+//			System.out.println(num+" "+num1);
+		
+		
+	}
+	
+	public static void main(String args[])
+	{
+		int [] arr = {1,3,5,8,9,16,20};
+		n2(arr);
+	}
+
+}
--- src/Complexity/Example1.java
+++ src/Complexity/Example1.java
@@ -26,6 +26,20 @@ public class Example1 {
 			// Note down that 0(n) or 0(n+2) or 0(n+n)= 0(2n) are equal to 0(n) if the user input is very large....
 		}
 		
+		// case 3 
+		public static  void case3(int [] num, String[] name)
+		{
+			 // overall complexity is 0(n+m) --> 0(n)
+			for(int numbers : num)  // run time complexity 0(n)
+			{
+				System.out.println(numbers);
+			}
+			
+			for(String names: name)			// run time complexity 0(m)
+			{
+				System.out.println(names);
+			}
+		}
 		
 	public static void main(String[] args) {
 
