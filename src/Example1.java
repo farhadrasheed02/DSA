317f1e6013707abc83f3bba5a1433cb5eb7dfde7
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-18 21:16:01 +0530

    Commit By FR

--- src/Example1.java
+++ src/Example1.java
@@ -3,6 +3,8 @@ public class Example1 {
 		
 		public void print(int [] num)
 		{
+			// the output will not depend on the input as it will always print first index, so have constant time
+			// we can say then it have time complexity as    0(1)  --> run time complexity  constant time..
 			System.out.println(num[0]);
 			
 		}
