69153bcb7233a4b19270f31dc6f4c681d69afd4e
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-19 23:54:44 +0530

    Commit By FR

---
+++ src/Complexity/SpaceComplexity.java
@@ -0,0 +1,17 @@
+package Complexity;
+
+public class SpaceComplexity {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		for(int i=0;i<args.length;i++)
+		{
+			System.out.println(args[i]);
+			
+		}
+			
+	}
+
+}
