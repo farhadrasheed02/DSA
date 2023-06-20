0bc1dc6cd957044402f54506aacbf8d8c520986c
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-21 01:32:15 +0530

    Commit By FR

--- src/Complexity/DynamicArray.java
+++ src/Complexity/DynamicArray.java
@@ -1,8 +1,64 @@
 package Complexity;
 
-public class DynamicArray {
-	public DynamicArray(int [] items)
-	{
+public class DynamicArray{
+	 private int  [] items;
+	 private int count;
 	
+	public DynamicArray(int length)
+	{
+		items = new int[length];
+	}
+	
+	public int indexOf(int indxitem)  // search operation ....
+	{
+		 for(int i=0;i<count;i++)
+		 {
+			if(items[i]==indxitem)
+				{return i;
+				}
+		 }
+		 return -1;
+	}
+	
+	public void insert(int item)
+	{
+		// if Array is full we will make new array double the size of items array and copy the element in it
+		if(items.length==count) {
+			int [] newitems = new int[count*2];   // count *2 double the size of first array i.e. items
+		
+			for(int i=0;i<count;i++)
+				newitems[i] = items[i];  // copying all existing elements...
+			
+			items=newitems;  // set items of this new array...
+		}
+		  
+		
+		
+		
+		
+		// we will add item at the end of the array, for now the size of the array is count so when we add the item it goes to count++ and so on...
+		items[count++] = item;
+	
+	}
+
+	public void removeAt(int index)
+	{
+		//validate the index
+		if(index<0 || index>=count)
+		{
+			throw new IllegalArgumentException();
+		}	
+			// shift the element to the left ...
+			for(int i=index;i<count;i++)
+				{items[i]=items[i+1];
+				}
+		count --;
+	}
+	public void print()
+	{
+		for(int i=0;i<count;i++)  // if we uses items.length --> it will take the memory you can use to check the difference....
+		{
+			System.out.println(items[i]);
+		}
 	}
 }
--- src/Complexity/DynamicMain.java
+++ src/Complexity/DynamicMain.java
@@ -4,7 +4,14 @@ public class DynamicMain {
 
 	public static void main(String[] args) {
 		
-		DynamicArray darray = new DynamicArray();
+		DynamicArray darray = new DynamicArray(3);
+		darray.insert(10);
+		darray.insert(20);
+		darray.insert(30);
+		darray.insert(40);
+		//darray.removeAt(2);
+		System.out.println(darray.indexOf(100));
+		darray.print();
 	}
 
 }
