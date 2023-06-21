51b6423c17526347cb210e88ec034c836a0b333c
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-22 00:03:18 +0530

    Commit By FR

--- src/LinkedList/LinkedListClass.java
+++ src/LinkedList/LinkedListClass.java
@@ -1,5 +1,36 @@
 package LinkedList;
 
 public class LinkedListClass {
-
+	
+	private class Node {
+		private int value;
+		private Node next;
+		public Node(int value)
+		{
+			this.value=value;
+		}
+	}
+	private Node first;
+	private Node last;
+	
+	public void addLast(int item)
+	{
+		var node = new Node(item);
+		if(first==null) // if no elemet 
+			first=last=node;
+		else
+			last.next=node;
+		last = node;
+	}
+	public void print() {
+        Node current = first;
+        
+        System.out.print("LinkedList: ");
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+	
 }
--- src/LinkedList/MainClass.java
+++ src/LinkedList/MainClass.java
@@ -4,7 +4,12 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		var lst = new LinkedListClass();
+		lst.addLast(10);
+		lst.addLast(20);
+		lst.addLast(30);
+		lst.print();
 	}
 
 }
