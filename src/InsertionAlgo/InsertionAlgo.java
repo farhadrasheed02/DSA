a8d294ec079fb49eb603616f144acbe9a8b878ba
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-24 23:24:35 +0530

    Commit By FR

--- src/InsertionAlgo/InsertionAlgo.java
+++ src/InsertionAlgo/InsertionAlgo.java
@@ -1,10 +1,31 @@
 package InsertionAlgo;
 
+import java.util.Arrays;
+
 public class InsertionAlgo {
 
+		public static void sort(int[] array)
+		{
+			for(var i=1;i<array.length;i++) {
+				var current = array[i];
+			
+			var  j=i-1;
+			while( j>=0 && array[j]>current)
+			{
+				array[j+1] =array[j];
+			j--;	
+			}
+			array[j+1] =current;
+			}	
+			
+		}
 	public static void main(String args[])
 	{
 		// Insertion can be imagined like playing a card game...
+		
+		int [] arr = {8,7,6};
+		sort(arr);
+		System.out.println(Arrays.toString(arr));
 		
 	}
 }
