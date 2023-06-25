a5c0a02d1ca9152657e47deb75ff654e93e9848e
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-25 17:09:22 +0530

    Commit By FR

---
+++ src/InsertionAlgo/practice.java
@@ -0,0 +1,22 @@
+package InsertionAlgo;
+
+import java.util.Arrays;
+
+public class practice {
+
+	public static void inser(int arr[])
+	{ 
+	var i =0;
+		 for(i=0;i<arr.length;i++)
+		 {
+			 
+		 }
+	}
+		
+	public static void main(String args[])
+	{
+		int arr [] = {11,5,8,6,4,3};
+		inser(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+}
