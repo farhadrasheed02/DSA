580300622de76020a99ed78b001d4a4d15051ab7
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-27 00:34:06 +0530

    Commit By FR

--- src/Bubblesort/BubbleSorting.java
+++ src/Bubblesort/BubbleSorting.java
@@ -4,26 +4,39 @@ import java.util.Arrays;
 
 public class BubbleSorting {
 	public  static void bsort(int[]arr)
-	{
-		var i=0;
-		for(i=1;i<arr.length+1;i++)
+	{//int temp=0;
+		for(int i=1;i<arr.length;i++)
 		{
-			
 		for(int j=1;j<arr.length;j++)
 		{
-			if(arr[j]<arr[j-1]) 
-				swap(arr,j,j-1);
-			
+			if(arr[j]<arr[j-1])
+			{int temp = arr[j-1];
+			arr[j-1]	= arr[j];
+			arr[j]=temp;
+			}
 		}
 		}
-	}
-	private static void swap(int array[], int index1, int index2)
-	{
-		int temp = array[index1];
-		array[index1]=array[index2];
-		array[index2]=temp;
 		
 	}
+//		var i=0;
+//		for(i=1;i<arr.length+1;i++)
+//		{
+//			
+//		for(int j=1;j<arr.length;j++)
+//		{
+//			if(arr[j]<arr[j-1]) 
+//				swap(arr,j,j-1);
+//			
+//		}
+//		}
+	
+//	private static void swap(int array[], int index1, int index2)
+//	{
+//		int temp = array[index1];
+//		array[index1]=array[index2];
+//		array[index2]=temp;
+		
+//	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
